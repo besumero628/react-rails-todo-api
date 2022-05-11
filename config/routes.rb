11baa84c38ee0746c1fb434a74e7b18304768629
NaraSyuhei <syuhei.nara@novel.jpn.com>
2022-05-11 21:47:13 +0900

    [Add] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :v1 do
+    resources :todos, only:[:index, :create, :destroy]
+    resources :users, only:[:index, :create]
+  end
 end
